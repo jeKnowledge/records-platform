416b70d8dc37804d6b1d95a14a7dc47f5bb85e20
epreto <edmpreto@gmail.com>
2014-05-23 03:59:47 +0100

    invite to meeting

--- client/main.js
+++ client/main.js
@@ -1,1 +1,2 @@
 Meteor.subscribe('meetings');
+Meteor.subscribe("directory");
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
 Meteor.publish ('meetings', function() {
  return Meetings.find({});
 });
+
+Meteor.publish ("directory", function() {
+	return Meteor.users.find({}, {fields: {emails: 1, profile:1}});
+});
