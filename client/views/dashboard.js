e0b759dd40441e8f0e845840a91907d074da7a55
epreto <edmpreto@gmail.com>
2014-05-23 01:00:56 +0100

    add new meeting

--- client/views/dashboard.js
+++ client/views/dashboard.js
@@ -4,7 +4,13 @@ Template.dashboard.meetings = function () {
 
 Template.dashboard.events({
 	'click #new-meeting-btn': function() {
-		Meteor.call('newMeeting', function(error) {
+		var addName =$ ('#add-meeting-name-input').val(); 
+		var addDate =$ ('#add-meeting-date-input').val();
+		var addTime =$ ('#add-meeting-time-input').val();
+		var addSubject =$ ('#add-meeting-subject-input').val();
+
+
+		Meteor.call('newMeeting', addName, addDate, addTime, addSubject, function(error) {
 		if (error) {
 			console.log(error);
 		}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 Meteor.methods({
-	newMeeting: function () {
-		Meetings.insert({ name:"Nova"});
-		console.log("Sucesso");
+	newMeeting: function (addName, addDate, addTime, addSubject) {
+		Meetings.insert({ name: addName, date: addDate, time: addTime, subject: addSubject});
+		console.log("Sucesso a criar nova reunião");
 	}
 });
