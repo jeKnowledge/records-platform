70948f26b975fda42fa6dae39748f73b14e102d8
João Bernardo <jb.amaro@hotmail.com>
2014-08-08 23:29:18 +0100

    Display selected ata info

--- client/views/manageatas.js
+++ client/views/manageatas.js
@@ -1,3 +1,26 @@
-Template.manageatas.atasubject = function () {
-  return Atas.find({ });
+var updateForm = function (subject, date, department, content) {
+  console.log(date);
+  $('#edit-ata-subject').val(subject);
+  $('#edit-ata-date').val(date);
+  $('#edit-ata-department').val(department);
+  $('#edit-ata-content').val(content);  
+};
+
+Template.manageatas.rendered = function () {
+    $('#edit-ata-date').datepicker();
+    var firstAta = Atas.findOne();
+    updateForm(firstAta.subject, firstAta.date, firstAta.department, firstAta.content);
 }
+
+Template.manageatas.atasubject = function () {
+  return Atas.find();
+}
+
+Template.manageatas.events({
+  'change #ata-selector': function (evt, tmpl) {
+    //May broke up if two atas have the same name (fix later)
+    var selectedAta = $('#ata-selector').val();
+    var searchAta = Atas.find({ subject: selectedAta }).fetch()[0];
+    updateForm(searchAta.subject, searchAta.date, searchAta.department, searchAta.content);
+  }
+});
