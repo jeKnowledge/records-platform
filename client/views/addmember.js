15aeb7df80e1112e714faee01970c186bd234b3c
João Bernardo <jb.amaro@hotmail.com>
2014-08-13 22:38:13 +0100

    Add option to add members to array

--- client/views/addmember.js
+++ client/views/addmember.js
@@ -1,3 +1,27 @@
 Template.addmember.members = function () {
   return Members.find();
 }
+
+Template.addmember.rendered = function () {
+  Session.set('added', []);
+}
+
+Template.addmember.addedm = function () {
+  return Session.get('added');
+}
+
+Template.addmember.events({
+  'click #add-member': function (evt, tmpl) {
+    var membro_id = $('#members-select option:selected').attr('name');
+
+    if (membro_id === 'empty') {
+      //display message say select member please
+    } else {
+      var membro = Members.find({ _id: membro_id }).fetch()[0];
+      var array = Session.get('added');
+      array[array.length] = membro;
+
+      Session.set('added', array);
+    }
+  }
+});
