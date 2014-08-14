c83758dff85c089114f14a0c754cd35da4e78e65
João Bernardo <jb.amaro@hotmail.com>
2014-08-14 16:08:34 +0100

    Add manage members form

--- client/views/admin/managemembers.js
+++ client/views/admin/managemembers.js
@@ -13,6 +13,10 @@ Template.managemembers.rendered = function () {
   $('#info-paragraph').hide();
 }
 
+Template.managemembers.membros = function () {
+  return Members.find();
+}
+
 Template.managemembers.events({
   'submit #new-member-form': function (evt, tmpl) {
     evt.preventDefault();      
@@ -20,7 +24,7 @@ Template.managemembers.events({
     var email = $('#member-email').val();
     var departamento = $('#member-department').val();
     
-    Meteor.call('addMember', nome, email, departamento, function (error) {
+    Meteor.call('addmember', nome, email, departamento, function (error) {
       if (error) {
         displayMessage('alert-danger', error);
       } else {
@@ -29,5 +33,53 @@ Template.managemembers.events({
         $('#member-email').val('');
       }
     });
+  }, 
+  'change #membro-selector': function (evt, tmpl) {
+    var selectedMember = $('#membro-selector option:selected').attr('name');
+
+    if (selectedMember === 'empty') {
+      $('#edit-member-name').val('');
+      $('#edit-member-email').val('');      
+    } else {
+      var searchMember = Members.find({ _id: selectedMember }).fetch()[0];
+      $('#edit-member-name').val(searchMember.name);
+      $('#edit-member-email').val(searchMember.email);
+      $('#edit-member-department').val(searchMember.department);      
+    }
+  },
+  'click #update-member': function () {
+    var memberId = $('#membro-selector option:selected').attr('name');
+    var nome = $('#edit-member-name').val();
+    var email = $('#edit-member-email').val();
+    var department = $('#edit-member-department').val(); 
+
+    if (memberId === 'empty') {
+      displayMessage('alert-info', 'Escolha um membro primeiro.');
+    } else {
+      Meteor.call('updatemember', memberId, nome, email, department, function (error) {
+        if (error) {
+          displayMessage('alert-danger', error);
+        } else {
+          displayMessage('alert-success', 'Membro atualizado com sucesso.');
+        }
+      });
+    }
+  },
+  'click #delete-member': function (evt, tmpl) {
+    var memberId = $('#membro-selector option:selected').attr('name');
+    
+    if (memberId === 'empty') {
+      displayMessage('alert-info', 'Escolha um membro primeiro.');
+    } else {
+      Meteor.call('deletemember', memberId, function (error) {
+        if (error) {
+          displayMessage('alert-danger', error);
+        } else {
+          displayMessage('alert-success', 'Membro eliminado com sucesso.');
+          $('#edit-member-name').val('');
+          $('#edit-member-email').val('');
+       }
+      });
+    }
   }
 }); 
--- server/adminMethods.js
+++ server/adminMethods.js
@@ -4,7 +4,7 @@ function validateEmail(email) {
 } 
 
 Meteor.methods({
-  'addMember': function (member_name, member_email, member_department) {
+  'addmember': function (member_name, member_email, member_department) {
     if (!member_name) {
       throw new Meteor.Error(411, 'Member name is too small.');
     }
@@ -14,5 +14,24 @@ Meteor.methods({
     }
     
     Members.insert({ name: member_name, email: member_email, department: member_department });
+  },
+  'updatemember': function (member_id, member_name, member_email, member_department) {
+    if (!member_name) {
+      throw new Meteor.Error(411, 'Member name is too small.');
+    }
+
+    if (!validateEmail(member_email)) {
+      throw new Meteor.Error(400, 'Email is not valid.');
+    }
+
+    Members.update({ _id: member_id},
+                   { name: member_name, email: member_email, department: member_department});
+  },
+  'deletemember': function (member_id) {
+    Members.remove({ _id: member_id }, function (error) {
+      if (error) {
+        throw new Meteor.Error(500, error);
+      }
+    })
   }
 });
