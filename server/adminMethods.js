35dc813a088df01a0aaeb29a31304c0b2c66cfb9
João Bernardo <jb.amaro@hotmail.com>
2014-08-14 10:19:30 +0100

    Add email validation function

--- server/adminMethods.js
+++ server/adminMethods.js
@@ -1,4 +1,7 @@
-//function to check if email is valid
+function validateEmail(email) { 
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+} 
 
 Meteor.methods({
   'addMember': function (member_name, member_email, member_department) {
@@ -6,8 +9,7 @@ Meteor.methods({
       throw new Meteor.Error(411, 'Member name is too small.');
     }
 
-    //use functio to check if email is valid 
-    if (false) {
+    if (!validateEmail(member_email)) {
       throw new Meteor.Error(400, 'Email is not valid.');
     }
     
