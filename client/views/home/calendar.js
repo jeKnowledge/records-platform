3ec365ea63769037c79da2133fbd51c8bc194525
João Bernardo <jb.amaro@hotmail.com>
2014-08-29 14:04:09 +0100

    Display meeting info when meeting event is clicked

--- client/views/home/calendar.js
+++ client/views/home/calendar.js
@@ -1,3 +1,21 @@
+Template.calendar.helpers({
+  project: function () {
+    return Session.get('mi_project');
+  },
+  date: function () {
+    return Session.get('mi_date');
+  },
+  time: function () {
+    return Session.get('mi_time');
+  },
+  department: function () {
+    return Session.get('mi_department');
+  },
+  description: function () {
+    return Session.get('mi_description');
+  }
+})
+
 Template.calendar.rendered = function () {
   Meteor.call('generateEvents', function (error, data) {
     if (error) {
@@ -5,12 +23,22 @@ Template.calendar.rendered = function () {
     } else {
       $('#calendar').fullCalendar({
         events: data,
+
         eventClick: function(calEvent, jsEvent, view) {
           event_f = Meetings.find({ _id: calEvent.db_id }).fetch()[0];
 
-          console.log(event_f);
+          Session.set('mi_project', Projects.find({ _id: event_f.project }).fetch()[0].name);
+          Session.set('mi_date', event_f.date);
+          Session.set('mi_time', event_f.time);
+          Session.set('mi_department', event_f.department);
+          Session.set('mi_description', event_f.description);          
+          
+          //Show Modal
+          $('#meeting-info').modal('show');
         }
       });
     }
   });
 }
+
+
