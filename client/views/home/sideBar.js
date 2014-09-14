f63e1d4473faa40b8d851a5e96ab814c5e378ceb
João Bernardo <jb.amaro@hotmail.com>
2014-09-14 22:02:06 +0100

    Show meeting info modal when meeting is selected from sidebar

--- client/views/home/sideBar.js
+++ client/views/home/sideBar.js
@@ -29,6 +29,22 @@ Template.sideBar.meetings = function () {
 
 Template.sideBar.events({
   'click .meeting-list-item': function (evt, tmpl) {
+    event_f = Meetings.find({ _id: this._id }).fetch()[0];
+
+    Session.set('current_meeting', this._id);
+    Session.set('mi_project', Projects.find({ _id: event_f.project }).fetch()[0].name);
+    Session.set('mi_date', event_f.date);
+    Session.set('mi_time', event_f.time);
+    Session.set('mi_department', event_f.department);
+    Session.set('mi_description', event_f.description);
+
+    var array = [];
+    for (var i = 0; i < event_f.members.length; i++) {
+      array[array.length] = Meteor.users.find({ _id: event_f.members[i] }).fetch()[0];
+    };
+    Session.set('mi_members', array);
+   
     //Show Modal
+    $('#meeting-info').modal('show'); 
   }
 })
